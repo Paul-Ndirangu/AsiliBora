bf6a4c8d4731afa724ae55e214f6d056d2f721c3
Paul-Ndirangu <paulmwaura254@gmail.com>
2023-07-22 10:38:04 +0300

    .

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,28 @@
 import os
 from thinkgpt.llm import ThinkGPT
+from fastapi import FastAPI
+
+app = FastAPI()
 
 os.environ['OPENAI_API_KEY'] = "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"
 
-llm = ThinkGPT(model_name="gpt-3.5-turbo", openai_api_key=os.environ['OPENAI_API_KEY'])
 
-rules = llm.abstract(observations=[
-    "in swahili, I did not eat is \"sikula\"",
-    "I did not work is \"ma sikufanya kazi\"",
-    "I did not go is \"sikuenda\"",
-], instruction_hint="output the rule in french")
-llm.memorize(rules)
+@app.post("/learn")
+async def learn():
+    llm = ThinkGPT(model_name="gpt-3.5-turbo", openai_api_key=os.environ['OPENAI_API_KEY'])
 
-llm.memorize("in swahili, I studied is \"nilisoma\"")
+    rules = llm.abstract(observations=[
+        "in swahili, I did not eat is \"sikula\"",
+        "I did not work is \"ma sikufanya kazi\"",
+        "I did not go is \"sikuenda\"",
+    ], instruction_hint="output the rule in french")
+    llm.memorize(rules)
 
-task = "translate to Tunisian: I didn't study"
-llm.predict(task, remember=llm.remember(task))
+    llm.memorize("in swahili, I studied is \"nilisoma\"")
+
+    task = "translate to Tunisian: I didn't study"
+    llm.predict(task, remember=llm.remember(task))
 
 
+if __name__ == "__main__":
+    uvicorn.run(app)
